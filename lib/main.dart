c00552de70fb3de40315988052feb0f0f32d462c
Abdullah Shaikh <153055241+Abdullah-Shaikh45@users.noreply.github.com>
2025-02-02 12:47:27 +0530

    Configured firebase for the PathForge

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gemini/flutter_gemini.dart';
+import 'package:path_forge/firebase_options.dart';
 import 'package:path_forge/screens/dash_board.dart';
 import 'package:path_forge/screens/onboarding_screen.dart';
 import 'package:path_forge/secrets/gemini_key.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   Gemini.init(apiKey: GEMINI_API_KEY);
   runApp(const MyApp());
 }
